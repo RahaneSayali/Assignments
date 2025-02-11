d6a48b6baaa966aee1451d724f7a8f24b02f06d6
Sayali Rahane <sayalipr15@gmail.com>
2025-02-11 14:53:51 +0530

    delete review part

--- Assignment 6/src/service/reviewService.ts
+++ Assignment 6/src/service/reviewService.ts
@@ -1,6 +1,6 @@
 import { fetchBookFromExternalAPI } from "../utils/FetchExternalBooks";
 import { Review, User, Book } from "../models/associations"; // Adjust the path based on your file structure
-
+import { Op } from "sequelize";
 export const addReview = async (
   bookId: string,
   userId: string,
@@ -34,5 +34,29 @@ export const getReviewsByBookId = async (bookId: string) => {
     return { reviews, externalBookData };
   } catch (error) {
     throw new Error("failed to fetch reviews : " + error);
+  }
+};
+
+export const deleteReviewById = async (reviewId: string, userId: string) => {
+  try {
+    // Fetch the review to check if the user is the owner or admin
+    const review = await Review.findByPk(reviewId, {
+      include: [{ model: User, attributes: ["id"] }], // Include the user who created the review
+    });
+
+    if (!review) {
+      throw new Error("Review not found");
+    }
+
+    // Check if the user is the author or an admin
+    if (review.userId === userId || (req as any).user.role === "admin") {
+      // Proceed to delete the review
+      await review.destroy();
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    throw new Error("Failed to delete review: " + error);
   }
 };
